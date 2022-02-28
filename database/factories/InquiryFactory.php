24dad39792e279f3f70b6ff53c831f58d08d9d1b
Rovshen Annayev <81748081+rovshena@users.noreply.github.com>
2022-02-28 13:56:33 +0500

    inquiry factory

---
+++ database/factories/InquiryFactory.php
@@ -0,0 +1,37 @@
+<?php
+
+namespace Database\Factories;
+
+use App\Models\Inquiry;
+use Illuminate\Database\Eloquent\Factories\Factory;
+
+class InquiryFactory extends Factory
+{
+    protected $model = Inquiry::class;
+
+    /**
+     * Define the model's default state.
+     *
+     * @return array
+     */
+    public function definition()
+    {
+        return [
+            'name' => $this->faker->name,
+            'email' => $this->faker->freeEmail,
+            'phone' => $this->faker->e164PhoneNumber,
+            'content' => $this->faker->paragraphs(20, true),
+            'is_read' => $this->faker->numberBetween(0, 1),
+            'created_at' => $this->faker->dateTimeThisMonth('now', config('app.timezone'))
+        ];
+    }
+
+    public function unread()
+    {
+        return $this->state(function (array $attributes) {
+            return [
+                'is_read' => 0,
+            ];
+        });
+    }
+}
