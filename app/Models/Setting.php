04c39b5ac44a45d8735e8d1cde076b951cb95ed3
Rovshen Annayev <81748081+rovshena@users.noreply.github.com>
2022-03-05 15:23:54 +0500

    setting resource

--- app/Models/Setting.php
+++ app/Models/Setting.php
@@ -2,12 +2,14 @@
 
 namespace App\Models;
 
+use App\Traits\HasStatusAttribute;
+use App\Traits\HasStatusScope;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 
 class Setting extends Model
 {
-    use HasFactory;
+    use HasFactory, HasStatusAttribute, HasStatusScope;
 
     protected $guarded = ['id'];
 }
