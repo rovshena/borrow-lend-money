9ffa5daad2e4d3266f71dc2bec246c475f2af80a
Rovshen Annayev <81748081+rovshena@users.noreply.github.com>
2022-03-05 15:59:29 +0500

    web routes

--- routes/web.php
+++ routes/web.php
@@ -1,5 +1,13 @@
 <?php
 
+use App\Http\Controllers\Admin\InquiryController;
+use App\Http\Controllers\Admin\SettingController;
+use App\Http\Controllers\Admin\UserController;
+use App\Http\Controllers\Auth\AdminAuthenticationController;
+use App\Http\Controllers\Auth\AdminProfileController;
+use App\Http\Controllers\HomeController;
+use App\Http\Controllers\LocalizationController;
+use App\Http\Controllers\SiteController;
 use Illuminate\Support\Facades\Route;
 
 /*
@@ -13,6 +21,34 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::get('/', function () {
-    return view('welcome');
+Route::get('/', [HomeController::class, 'index'])->name('index');
+Route::get('locale/{locale}', LocalizationController::class)->name('locale');
+
+Route::get('/privacy-policy', [SiteController::class, 'privacy'])->name('privacy');
+Route::get('/terms-of-use', [SiteController::class, 'terms'])->name('terms');
+Route::get('/about', [SiteController::class, 'about'])->name('about');
+Route::view('/contact', 'visitor.site.contact')->name('contact');
+Route::put('/contact', [SiteController::class, 'contact'])->name('contact.post')->middleware('throttle:contact');
+
+Route::group(['prefix' => 'admin', 'as' => 'admin.'], function () {
+
+    Route::middleware('guest:admin')->group(function () {
+        Route::get('/login', [AdminAuthenticationController::class, 'create'])->name('login');
+        Route::post('/login', [AdminAuthenticationController::class, 'store'])->name('login.store');
+    });
+
+    Route::middleware('auth:admin')->group(function () {
+        Route::post('/logout', [AdminAuthenticationController::class, 'destroy'])->name('logout');
+        Route::get('/change-password', [AdminProfileController::class, 'showChangePasswordForm'])->name('change-password');
+        Route::put('/change-password', [AdminProfileController::class, 'changePassword'])->name('change-password.update');
+        Route::get('/profile', [AdminProfileController::class, 'edit'])->name('profile');
+        Route::put('/profile', [AdminProfileController::class, 'update'])->name('profile.update');
+        Route::view('/', 'admin.index')->name('index');
+        Route::get('/inquiries', [InquiryController::class, 'index'])->name('inquiries');
+        Route::get('/inquiries/{inquiry}/', [InquiryController::class, 'show'])->name('inquiries.show');
+        Route::delete('/inquiries/{inquiry}/destroy', [InquiryController::class, 'destroy'])->name('inquiries.destroy');
+        Route::get('/inquiries/mark-all/as-read', [InquiryController::class, 'markAllAsRead'])->name('inquiries.mark-all-as-read');
+        Route::resource('settings', SettingController::class)->except(['create', 'store', 'destroy']);
+        Route::resource('users', UserController::class)->except(['show']);
+    });
 });
