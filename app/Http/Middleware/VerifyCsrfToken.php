580a9d0b0c2a2d9d64eb879a44793294aab4b3c9
Rovshen Annayev <81748081+rovshena@users.noreply.github.com>
2022-03-05 12:28:24 +0500

    middlewares

--- app/Http/Middleware/VerifyCsrfToken.php
+++ app/Http/Middleware/VerifyCsrfToken.php
@@ -2,7 +2,11 @@
 
 namespace App\Http\Middleware;
 
+use Illuminate\Contracts\Support\Responsable;
 use Illuminate\Foundation\Http\Middleware\VerifyCsrfToken as Middleware;
+use Illuminate\Http\Request;
+use Symfony\Component\HttpFoundation\Cookie;
+use Symfony\Component\HttpFoundation\Response;
 
 class VerifyCsrfToken extends Middleware
 {
@@ -14,4 +18,29 @@ class VerifyCsrfToken extends Middleware
     protected $except = [
         //
     ];
+
+    /**
+     * Add the CSRF token to the response cookies.
+     *
+     * @param Request $request
+     * @param Response $response
+     * @return Response
+     */
+    protected function addCookieToResponse($request, $response)
+    {
+        $config = config('session');
+
+        if ($response instanceof Responsable) {
+            $response = $response->toResponse($request);
+        }
+
+        $response->headers->setCookie(
+            new Cookie(
+                'XSRF-TOKEN', $request->session()->token(), $this->availableAt(60 * $config['lifetime']),
+                $config['path'], $config['domain'], $config['secure'], $config['http_only'], false, $config['same_site'] ? $config['same_site'] : null
+            )
+        );
+
+        return $response;
+    }
 }
